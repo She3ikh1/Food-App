5bef7ae6329b3447bb749c1e88fd09d00552ea16
Sheikh Yasir <96722288+She3ikh1@users.noreply.github.com>
2025-01-09 22:11:32 +0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,13 @@ const port=4000
 
 // middleware
 app.use(express.json())
-app.use(cors())
+app.use(cors(
+     {
+          origin:["https://Food-order-1whq.vercel.app"],
+          methods:["POST","GET"],
+          credentials:true
+     }
+            ));
 
 // dB connection
 
